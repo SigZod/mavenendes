e1b4e7c4eb1a21e0b03214c37c45272c0ca5279d
SstariusS <antonionies@hotmail.es>
2019-06-03 17:41:47 +0200

    modificamos maven

---
+++ README.md
@@ -0,0 +1,2 @@
+# maven-quick-start
+Projecto maven use quick
---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,12 @@
+package clinic.programming.training;
+
+public class Application{
+	public Application(){
+		System.out.println("Dentro de la aplicacion");
+	}
+
+	public static void main (String[] args){
+		System.out.println("Empezando aplicacion");
+		Application app = new Application();
+	}
+}

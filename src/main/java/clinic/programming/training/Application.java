108a988989d60675e2ca0e810b07c630f81d815c
SstariusS <antonionies@hotmail.es>
2019-06-03 21:59:02 +0200

    Usamos StringUtils y añadimos dependencias

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application{
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+
 
 	public Application(){
 		System.out.println("Dentro de la aplicacion");
@@ -13,6 +20,7 @@ public class Application{
 		System.out.println("Empezando aplicacion");
 		Application app = new Application();
 		System.out.println("Segundo java");
-		
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count: " + count);
 	}
 }
